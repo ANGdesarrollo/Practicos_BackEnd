9d7ac16cfdd8167216d0a08505e5022c113aea98
Alexis Graff <alexisgraff123@gmail.com>
2022-10-21 17:55:16 -0300

    eliminacion de codigo repetido

--- practico-3/server.js
+++ practico-3/server.js
@@ -8,7 +8,6 @@ function createSv() {
     app.get("/products", async (req,res) => {
         const container = new classContainer('products.txt')
         const allProducts = await container.getAll()
-        Math.random()
         res.json(allProducts)
     });
     app.get("/randomProduct", async (req,res) => {
