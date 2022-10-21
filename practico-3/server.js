e7756856593d938db6a08cdf57d11ef580a19146
Alexis Graff <alexisgraff123@gmail.com>
2022-10-21 17:49:38 -0300

    practico 3

---
+++ practico-3/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT | 8080
+const classContainer = require("./classContainer");
+
+
+function createSv() {
+    app.get("/products", async (req,res) => {
+        const container = new classContainer('products.txt')
+        const allProducts = await container.getAll()
+        Math.random()
+        res.json(allProducts)
+    });
+    app.get("/randomProduct", async (req,res) => {
+        const container = new classContainer('products.txt')
+        const allProducts = await container.getAll()
+        const randomProduct = allProducts.find(el => el.id === (Math.floor(Math.random() * allProducts.length)))
+        res.json(randomProduct)
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+createSv()
+
